880875dee1426436242c8fb7b17d6f9be825dd5e
Matheus Iurk Pedroso <mattiurkpedroso@gmail.com>
2024-06-19 16:50:03 -0300

    Update saudacao.js

--- Modulo1/saudacao.js
+++ Modulo1/saudacao.js
@@ -1,4 +1,5 @@
-var nome = 'Matheus teste'
-let mensagem = 'olá '
+var nome = 'Matheus Iurk'
+let mensagem = 'olá, prazer em te conhecer, me chamo'
 let saudacao = mensagem + nome
 console.log(saudacao)
+
